968a358047c223131ba0e6409cfc2eea128c4ea0
Yaroslav Slavinskiy <7blackice_@Yaroslavs-MacBook-Pro.local>
2025-02-08 00:05:17 +0300

    убрал лишний путь к 404

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import styles from './app.module.css';
-import { FourOFour } from './components/FourOFour';
 import { RefreshComponent } from './components/RefreshTodo';
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <div className={styles.app}>
       <Routes>
         <Route path="*" element={<RefreshComponent />} />
-        <Route path="/404" element={<FourOFour />} />
       </Routes>
     </div>
   );
